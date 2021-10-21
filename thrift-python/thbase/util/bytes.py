0b14c448232a9aa616fb9daac9cec36b39311e39
Yutong Sean <yutongsean@gmail.com>
2021-10-21 10:56:55 +0800

    Whitespace fix

--- thrift-python/thbase/util/bytes.py
+++ thrift-python/thbase/util/bytes.py
@@ -15,12 +15,12 @@ from sys import version_info
 
 
 def to_bytes(origin):
-	if version_info.major == 3:
+    if version_info.major == 3:
         return bytes(origin, 'utf-8') if isinstance(origin, str) else origin
     return bytes(origin) if isinstance(origin, str) else origin
 
 
 def to_str(origin):
-	if version_info.major == 3:
+    if version_info.major == 3:
         return origin.decode('utf-8') if isinstance(origin, bytes) else origin
     return str(origin) if isinstance(origin, bytes) else origin
