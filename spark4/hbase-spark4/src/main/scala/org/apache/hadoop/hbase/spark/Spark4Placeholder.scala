3fd2bf6ef39403b56f471f8b9f8c27980d75ea07
Wellington Ramos Chevreuil <wchevreuil@apache.org>
2026-05-21 15:51:07 +0100

    HBASE-30179: Define spark4 module and redefine common modules

---
+++ spark4/hbase-spark4/src/main/scala/org/apache/hadoop/hbase/spark/Spark4Placeholder.scala
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hbase.spark
+
+import org.apache.yetus.audience.InterfaceAudience
+
+/**
+ * Phase-0 build marker for the Spark 4 connector line (`hbase-spark4`).
+ * Replaced incrementally during feature porting; see ../../SPARK4-ROADMAP.md at repo root.
+ */
+@InterfaceAudience.Private
+private[hbase] object Spark4Placeholder
